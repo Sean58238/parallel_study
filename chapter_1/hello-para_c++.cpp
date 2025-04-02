5148e4a7030bd2795cd48f2269d0dc4ec524116c
root <root@example.com>
2025-04-02 18:10:12 +0800

    modify hello-para_c++.cpp to work

--- chapter_1/hello-para_c++.cpp
+++ chapter_1/hello-para_c++.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <string>
+// this header declares std::memcpy
+#include <cstring>
 
 using namespace std;
 
+// '{}' means list initialization or brace initialization (from C++11)
+// if you want to use C++98 initialization style, you can define:
+// const std::string secret = "...";
+// const std::string secret("...");
 const std::string secret {
     "Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
     "J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
@@ -10,13 +16,17 @@ const std::string secret {
 const auto sz = secret.size();
 
 int main(){
+    // allocate memory on heap
+    char* result = new char[sz];
 
     std::memcpy(result, secret.data(),  sz);
 
-    for(int i=0; i++; i<sz) {
+    for(int i=0; i<sz; i++) {
         result[i] -= 1;
     }
 
     std::cout << result << "\n";
+    // remember to release allocated memory
+    delete[] result;
     return 0;
 }
